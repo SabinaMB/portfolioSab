5284c428bce205d904241423147d1a7958d7e281
SabinaMB <sabina.marian.blaariu@gmail.com>
2024-04-19 00:45:01 +0200

    styling parts of hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import { getImageUrl } from '../../utils'
+import styles from './Hero.module.css'
 
 export const Hero = () => {
   return (
-    <section>
-        <div>
-            <h2>Delighted to make your acquaintance</h2>
-            <p>I'm Sabina, your Web & App developer</p>
-            <button>About me</button>
+    <section className={styles.container}>
+        <div className={styles.content}>
+            <h2 className={styles.title}>Delighted to make your acquaintance</h2>
+            <p className={styles.paragraph}>I'm Sabina, your Web & App developer</p>
+            <button className={styles.aboutBtn}>About me</button>
         </div>
+        <img src={getImageUrl('hero1.jpeg')} alt='hero section image' className={styles.heroImg}/>
 
     </section>
   )
