c99c91df7ccc99f5ffa3e9058e0621561bc198e2
SabinaMB <sabina.marian.blaariu@gmail.com>
2024-05-06 17:28:03 +0200

    started the Projects section

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import projects from '../../data/projects.json'
 
 export const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <section>
+        <h2>Projects</h2>
+        <div>
+            {projects.map((project, id) => {
+                return (
+                <div key={id}>
+                    <img src={project.imageSrc} alt={'Image of ${project.title}'}/>
+                    <h3>{project.title}</h3>
+                    <p>{project.description}</p>
+                    </div>
+            );
+            })}
+        </div>
+    </section>
+  );
+};
