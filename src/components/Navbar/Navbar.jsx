b059372876c44dfa8f25e38c0df1ccd55733c6be
SabinaMB <sabina.marian.blaariu@gmail.com>
2024-05-11 12:28:48 +0200

    made small corrections

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -31,9 +31,15 @@ export const Navbar = () => {
             <li>
                 <a href='#contact' className={styles.navLink}>Contact</a>
             </li>
-            <button className={styles.cvBtn}>
-            <a href="https://portfolio-sabina.netlify.app/CV%20Sabina%20MB%20Eng.pdf" target="_blank" rel="noopener noreferrer" className={styles.cvBtn}>Download CV</a>
-            </button> 
+            
+            <li className={styles.dropdownCV}>
+            <button className={styles.cvBtn}>Download CV</button>
+            <div className={styles.CV}>
+                <a href="https://portfolio-sabina.netlify.app/CV%20Sabina%20MB%20Eng.pdf" target="_blank" rel="noopener noreferrer" className={styles.cvContent}>Download CV English</a>
+                <a href="https://portfolio-sabina.netlify.app/CV%20Sabina%20MB%20De.pdf" target="_blank" rel="noopener noreferrer" className={styles.cvContent}>Download CV German</a>
+           </div>
+           
+             </li>
         </ul>
        
     </div>
