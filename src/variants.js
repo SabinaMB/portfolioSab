04ae5ae26370be3b17fccace1d37cf972e6a9be3
SabinaMB <sabina.marian.blaariu@gmail.com>
2024-11-17 16:00:54 +0100

    added framer motion variants

---
+++ src/variants.js
@@ -0,0 +1,29 @@
+export const fadeInLeft = {
+    hidden: { opacity: 0, x: -50 },
+    visible: { opacity: 1, x: 0 }
+};
+
+export const fadeInRight = {
+    hidden: { opacity: 0, x: 50 },
+    visible: { opacity: 1, x: 0 }
+};
+
+export const fadeInUp = {
+    hidden: { opacity: 0, y: 50 },
+    visible: { opacity: 1, y: 0 }
+};
+
+export const fadeInDown = {
+    hidden: { opacity: 0, y: -50 },
+    visible: { opacity: 1, y: 0 }
+};
+
+export const stagger = {
+    hidden: { opacity: 0 },
+    visible: {
+        opacity: 1,
+        transition: {
+            staggerChildren: 0.2    
+        }
+    }
+};
