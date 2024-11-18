42f8258cb450a9fb3e3b58b61127b7ac6c43489a
SabinaMB <sabina.marian.blaariu@gmail.com>
2024-11-18 15:16:59 +0100

    final touch navBar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -45,7 +45,7 @@ export const Navbar = () => {
         />
 
         <motion.ul 
-        key={isSmallScreen ? (menuOpen ? 'open' : 'closed'): 'largeScreen'}
+        // key={isSmallScreen ? (menuOpen ? 'open' : 'closed'): 'largeScreen'}
         className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
         onClick={() => setMenuOpen(false)}
         initial='hidden'
@@ -54,26 +54,24 @@ export const Navbar = () => {
         transition= {{duration: 1}}>
 
         <motion.li variants={fadeInDownBurger} className={styles.navLink}>
-            <a href='#about' className={styles.navLink}>About</a>
+        <a href='#about' className={styles.navLink}>About</a>
         </motion.li>
         <motion.li variants={fadeInDownBurger} className={styles.navLink}>
-                <a href='#skills' className={styles.navLink}>Skills</a>
+        <a href='#skills' className={styles.navLink}>Skills</a>
         </motion.li>
-                <motion.li variants={fadeInDownBurger} className={styles.navLink}>
-                <a href='#projects' className={styles.navLink}>Projects</a>
-                </motion.li>
-                <motion.li variants={fadeInDownBurger} className={styles.navLink}>
-                <a href='#contact' className={styles.navLink}>Contact</a>
-                </motion.li>
-            
-            <motion.li className={styles.dropdownCV}
-            variants={fadeInDownBurger}>
-            <a href='#cv' className={styles.navLink}>Download CV</a>
-            <div className={styles.CV}>
+        <motion.li variants={fadeInDownBurger} className={styles.navLink}>
+        <a href='#projects' className={styles.navLink}>Projects</a>
+        </motion.li>
+        <motion.li variants={fadeInDownBurger} className={styles.navLink}>
+        <a href='#contact' className={styles.navLink}>Contact</a>
+        </motion.li>
+        <motion.li className={styles.dropdownCV} variants={fadeInDownBurger}>
+        <a href='#cv' className={styles.navLink}>Download CV</a>
+        <div className={styles.CV}>
                 <a href="https://portfolio-sabina.netlify.app/CV_SabinaMB_Eng.pdf" target="_blank" rel="noopener noreferrer" className={styles.cvContent}>CV English</a>
                 <a href="https://portfolio-sabina.netlify.app/CV_SabinaMB_De.pdf" target="_blank" rel="noopener noreferrer" className={styles.cvContent}>CV German</a>
-            </div>
-            </motion.li>
+        </div>
+        </motion.li>
         </motion.ul>
        
     </div>
