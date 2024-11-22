92a5ed4abd3670e4acbac8e2b2f8c27c42312027
SabinaMB <sabina.marian.blaariu@gmail.com>
2024-11-22 14:41:28 +0100

    Finishing contact section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,16 +1,34 @@
 import React, { useState } from 'react';
 import styles from './Contact.module.css'
+import {motion} from 'framer-motion'
+
+const linkVariants = {
+  hidden: { opacity: 0, y: 100 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 1, ease: "easeOut" },
+  },
+};
 
 export const Contact = () => {
   return (
     <section className={styles.container} id='contact'>
        <div className={styles.contact}>
-        <a href="mailto:sabina_marian@yahoo.com" className={styles.link}>
+        <motion.a href="mailto:sabina_marian@yahoo.com" className={styles.link}
+         initial="hidden"
+         whileInView="visible"
+         viewport={{ once: true }}
+         variants={linkVariants}>
           Email me
-        </a>
-        <a href="https://www.linkedin.com/in/sabina-marian-blanariu-3276a3144/" target="_blank" rel="noreferrer" className={styles.link}>
+        </motion.a>
+        <motion.a href="https://www.linkedin.com/in/sabina-marian-blanariu-3276a3144/" target="_blank" rel="noreferrer" className={styles.link}
+         initial="hidden"
+         whileInView="visible"
+         viewport={{ once: true }}
+         variants={linkVariants}>
         Connect with me on LinkedIn
-        </a>
+        </motion.a>
        </div>
     </section>
   )
