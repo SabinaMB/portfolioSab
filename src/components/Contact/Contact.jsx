a8108bfd500c9fe1ff3da8d051085a7a11e5c566
SabinaMB <sabina.marian.blaariu@gmail.com>
2025-01-09 23:27:16 +0100

    new style for buttons

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import styles from './Contact.module.css'
-import {motion} from 'framer-motion'
+import React, { useState } from "react";
+import styles from "./Contact.module.css";
+import { motion } from "framer-motion";
 
 const linkVariants = {
   hidden: { opacity: 0, y: 100 },
@@ -13,24 +13,31 @@ const linkVariants = {
 
 export const Contact = () => {
   return (
-    <section className={styles.container} id='contact'>
-       <div className={styles.contact}>
-        <motion.a href="mailto:sabina_marian@yahoo.com" className={styles.link}
-         initial="hidden"
-         whileInView="visible"
-         viewport={{ once: true }}
-         variants={linkVariants}>
+    <section className={styles.container} id="contact">
+      <div className={styles.contact}>
+        <motion.a
+          href="mailto:sabina_marian@yahoo.com"
+          className={styles.link}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          variants={linkVariants}
+        >
           Email me
         </motion.a>
-        <motion.a href="https://www.linkedin.com/in/sabina-marian-blanariu-3276a3144/" target="_blank" rel="noreferrer" className={styles.link}
-         initial="hidden"
-         whileInView="visible"
-         viewport={{ once: true }}
-         variants={linkVariants}>
-        Connect with me on LinkedIn
+        <motion.a
+          href="https://www.linkedin.com/in/sabina-marian-blanariu-3276a3144/"
+          target="_blank"
+          rel="noreferrer"
+          className={styles.link}
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true }}
+          variants={linkVariants}
+        >
+          Connect with me on LinkedIn
         </motion.a>
-       </div>
+      </div>
     </section>
-  )
-}
-
+  );
+};
