90d5be99865b70dbea81178d36e6e1a70005f920
SabinaMB <sabina.marian.blaariu@gmail.com>
2025-01-18 14:19:58 +0100

    added visible to JSON

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,30 +1,31 @@
-
-import React from 'react';
-import { motion } from 'framer-motion';
-import { useInView } from 'react-intersection-observer';
-import projects from '../../data/projects.json';
-import { ProjectsCard } from './ProjectsCard';
-import styles from './Projects.module.css';
-import { fadeInLeft, fadeInContainer } from '../../variants';
+import React from "react";
+import { motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import projects from "../../data/projects.json";
+import { ProjectsCard } from "./ProjectsCard";
+import styles from "./Projects.module.css";
+import { fadeInLeft, fadeInContainer } from "../../variants";
 
 export const Projects = () => {
   const { ref, inView } = useInView({
-    threshold: 0.1, 
+    threshold: 0.1,
     triggerOnce: true,
-    id: 'projects',
+    id: "projects",
   });
 
+  const visibleProjects = projects.filter((project) => project.visible);
+
   return (
-    <section className={styles.container} id='projects'>
+    <section className={styles.container} id="projects">
       <h2 className={styles.title}>Projects</h2>
       <motion.div
         className={styles.project}
         ref={ref}
         variants={fadeInContainer}
         initial="hidden"
-        animate={inView ? 'visible' : 'hidden'}
+        animate={inView ? "visible" : "hidden"}
       >
-        {projects.map((project, id) => (
+        {visibleProjects.map((project, id) => (
           <ProjectItem key={id} project={project} />
         ))}
       </motion.div>
@@ -32,20 +33,18 @@ export const Projects = () => {
   );
 };
 
-
 const ProjectItem = ({ project }) => {
   const { ref, inView } = useInView({
-    triggerOnce: true, 
-    threshold: 0.2, 
+    triggerOnce: true,
+    threshold: 0.2,
   });
-
 
   return (
     <motion.div
-      ref={ref} 
+      ref={ref}
       variants={fadeInLeft}
       initial="hidden"
-      animate={inView ? 'visible' : 'hidden'}
+      animate={inView ? "visible" : "hidden"}
     >
       <ProjectsCard project={project} />
     </motion.div>
