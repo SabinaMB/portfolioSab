bf86929bb0861ec080bb96f90e5d8694da955a5e
SabinaMB <sabina.marian.blaariu@gmail.com>
2025-01-19 19:34:57 +0100

    changed About section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,24 +1,21 @@
-import React, { useRef, useEffect } from 'react';
-import {motion} from 'framer-motion';
-import {fadeInLeft, fadeInUp, fadeInRight, stagger} from '../../variants';
-import styles from './About.module.css';
-import { getImageUrl } from '../../utils';
-import animation from '../../../assets/scroll.json';
-import lottie from 'lottie-web';
-import { useInView } from 'react-intersection-observer';
-
-
+import React, { useRef, useEffect } from "react";
+import { motion } from "framer-motion";
+import { fadeInLeft, fadeInUp, fadeInRight, stagger } from "../../variants";
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+import animation from "../../../assets/scroll.json";
+import lottie from "lottie-web";
+import { useInView } from "react-intersection-observer";
 
 export const About = () => {
-
   const containerRef = useRef();
-  const aboutRef= useRef();
+  const aboutRef = useRef();
   let anim = null;
 
   useEffect(() => {
     const anim = lottie.loadAnimation({
       container: containerRef.current,
-      renderer: 'svg',
+      renderer: "svg",
       loop: true,
       autoplay: true,
       animationData: animation,
@@ -30,7 +27,6 @@ export const About = () => {
       }
     }, 10000);
 
-   
     return () => {
       if (anim) {
         anim.destroy();
@@ -39,83 +35,91 @@ export const About = () => {
     };
   }, []);
 
-const { ref: aboutMeRef, inView: aboutMeInView } = useInView({
-  triggerOnce: true,
-  threshold: 0.15,
-});
+  const { ref: aboutMeRef, inView: aboutMeInView } = useInView({
+    triggerOnce: true,
+    threshold: 0.15,
+  });
 
-const { ref: bootCampRef, inView: bootCampInView } = useInView({
-  triggerOnce: true,
-  threshold: 0.2,
-});
+  const { ref: bootCampRef, inView: bootCampInView } = useInView({
+    triggerOnce: true,
+    threshold: 0.2,
+  });
 
-const { ref: goalsRef, inView: goalsInView } = useInView({
-  triggerOnce: true,
-  threshold: 0.2,
-});
-
+  const { ref: goalsRef, inView: goalsInView } = useInView({
+    triggerOnce: true,
+    threshold: 0.2,
+  });
 
   return (
+    <motion.section
+      className={styles.container}
+      ref={aboutMeRef}
+      id="about"
+      initial="hidden"
+      animate={aboutMeInView ? "visible" : "hidden"}
+    >
+      <div className={styles.titleContainer}>
+        <p className={styles.title}>About me</p>
+      </div>
 
-    <motion.section className={styles.container} 
-    ref={aboutMeRef}
-    id='about'
-    initial="hidden" 
-    animate={aboutMeInView ? "visible" : 'hidden'}>
-         
-        <div className={styles.titleContainer}>
-         <p className={styles.title}>About me</p>
-        </div> 
-
-        <div className={styles.lottieWrapper}>
+      <div className={styles.lottieWrapper}>
         <div ref={containerRef} className={styles.lottieContainer}></div>
-        </div>
+      </div>
 
-        <div className={styles.path}>
-            <p className={styles.path_p}>
-            As an avid Web Developer and Backend Explorer, my journey has been shaped by a rich background in Project Management, Quality Assurance and Communication. 
-            </p>
-        </div>
-        
-        <motion.div className={styles.bootCamp} 
-        variants={stagger} 
+      <div className={styles.bCamp}>
+        <p className={styles.bCamp_p}>
+          I am a <strong>Web Developer</strong> passionate about{" "}
+          <strong>Frontend Development</strong> specializing in{" "}
+          <strong>React.js</strong> and <strong>JavaScript</strong>.<br /> After
+          completing a Full Stack Web Development bootcamp at WBS CODING SCHOOL,
+          I've continued to refine my skills through self-guided learning and
+          online courses.
+        </p>
+      </div>
+
+      <motion.div
+        className={styles.path}
+        variants={stagger}
         ref={bootCampRef}
-        initial="hidden" 
-        animate={bootCampInView ? "visible" : 'hidden'}
-        >
-            <motion.p className={styles.bootCamp_p} variants={fadeInLeft}>
-            Development Journey:
-            </motion.p>
-            <motion.p className={styles.bootCamp_p} variants={fadeInLeft} >
-            During a 15-week Full Stack Web and App Development course at WBS CODING SCHOOL in Berlin from October 2023 to February 2024, I delved deep into PERN and MERN stack applications. 
-            </motion.p>
-            <motion.p className={styles.bootCamp_p} variants={fadeInLeft} >
-            Collaborating weekly on coding challenges and completing 14 projects sharpened my skills, while also familiarizing me with Agile methodologies and Scrum practices.
-            </motion.p>
-            <motion.p className={styles.bootCamp_p} variants={fadeInLeft} >
-            'Scan & Serve' is a complex restaurant management app, developed by our 4 members team.  This project showcased my adaptability and problem-solving skills in a real-world scenario. 
-            </motion.p>
-        </motion.div>
+        initial="hidden"
+        animate={bootCampInView ? "visible" : "hidden"}
+      >
+        <motion.p className={styles.path_p} variants={fadeInLeft}>
+          With a background in Project Management and Quality Assurance, I bring
+          a wealth of experience in team collaboration, client communication,
+          and problem-solving.
+          <br /> My previous roles in academia and industry have equipped me
+          with skills that are highly relevant to this field.
+        </motion.p>
+      </motion.div>
 
-        <motion.div className={styles.goals} variants={fadeInUp} 
+      <motion.div
+        className={styles.goals}
+        variants={fadeInUp}
         ref={goalsRef}
         initial="hidden"
-        animate={goalsInView ? "visible" : 'hidden'}>
-          <p className={styles.goals_p} variants={fadeInUp}>
+        animate={goalsInView ? "visible" : "hidden"}
+      >
+        <p className={styles.goals_p} variants={fadeInUp}>
           Long-term Goals:
-          </p>
-          <p className={styles.goals_p} >
-          I aspire to contribute to innovative projects, leveraging my technical expertise and organizational prowess. 
-          </p>
-          <p className={styles.goals_p}>
-          My vision is to become an integral part of successful web development teams.
-          </p>
-        </motion.div>
-        
-        <figure className={styles.image}>
-        <img src={getImageUrl('imgSab8.jpg')} alt="Sabina" className={styles.imgSab}/>    
-        </figure>
+        </p>
+        <p className={styles.goals_p}>
+          I aspire to contribute to innovative projects, leveraging my technical
+          expertise and organizational prowess.
+        </p>
+        <p className={styles.goals_p}>
+          My vision is to become an integral part of successful web development
+          teams.
+        </p>
+      </motion.div>
 
+      <figure className={styles.image}>
+        <img
+          src={getImageUrl("imgSab8.jpg")}
+          alt="Sabina"
+          className={styles.imgSab}
+        />
+      </figure>
     </motion.section>
-  )
-}
+  );
+};
